f99bcf8724ce2161a3d87f49f326f7cd73bb281e
Shanmuga05 <priyasupramani1405@gmail.com>
2025-11-06 09:42:17 +0530

    springCore

---
+++ IOC/src/main/java/com/tnsif/IOC/IocApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.IOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocApplication.class, args);
+		System.out.println("shan");
+	}
+
+}
---
+++ IOC/src/main/java/com/tnsif/IOC/Mobile.java
@@ -0,0 +1,6 @@
+package com.tnsif.IOC;
+
+public interface Mobile {
+	void call();
+	void data();
+}
